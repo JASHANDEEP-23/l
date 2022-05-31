1d7e4b613cb4fd177a727a1f9bc3fd2bfa90c0a9
JASHANDEEP-23 <84918095+JASHANDEEP-23@users.noreply.github.com>
2022-05-31 14:16:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function next(){
+    window.location = "send.html"
+}
+function button1(){
+    document.getElementById("display_text").innerHTML="Mahatma Gandhi was the most popular freedom fighter and political leader of India. He initiated many freedom movements and rallied for the rights of farmers and untouchables. He was the main figure in the Indian freedom struggle. He was so much respected that he was called 'Bapu' or 'father' in India."
+}
+function button2(){
+    document.getElementById("display_text").innerHTML="APJ Abdul Kalam is a famous name in the whole world. He is counted among the greatest scientists of the 21st century. Even more, he becomes the 11th president of India and served his country. He was the most valued person of the country as his contribution as a scientist and as a president is beyond compare."
+}
+function button3(){
+    document.getElementById("display_text").innerHTML="Narendra Damodardas Modi is the present and 15th Indian prime minister. He has been serving our nation since 26th May 2014. From the year 2001 to 2014, before taking over Delhi, he served the role of Honourable Chief Minister of Gujarat. He is a Member of the Parliament (MP), who represents the city of Varanasi."
+}
+function button4(){
+    document.getElementById("display_text").innerHTML="Kalpana was the First spacewoman of India. It was the dream that many Indians dreamt of but only Kalpana was able to fulfill it. She had various ambitions in minds from her childhood only. Moreover, she always had an interest in aircraft, and because of that, she took aeronautical engineering."
+}
+function button5(){
+    document.getElementById("display_text").innerHTML="Indira Gandhi in India is known as the 'Iron lady of India'. After the Lal Bahadur Shastri died, Indira Gandhi became the prime minister if India. Also, she established her worth and became the strongest prime as well as president of the Congress party."
+}
+function button6(){
+    document.getElementById("display_text").innerHTML="Madurai Shanmukhavadivu Subbulakshmi (16 September 1916 – 11 December 2004) was an Indian Carnatic singer from Madurai, Tamil Nadu. She was the first musician ever to be awarded the Bharat Ratna, India's highest civilian honour."
+}
+function button7(){
+    document.getElementById("display_text").innerHTML="R. K. Narayan was born on 10 October 1906 and passed away in 2001. In his long career he published fourteen novels, over two hundred short stories, a memoir, two travel books, innumerable essays, and two plays. His first novel was Swami and Friends (1935)."
+}
+function button8(){
+    document.getElementById("display_text").innerHTML="Rani Lakshmi Bai, also known as Manikarnika Tambe, was born in a Brahmin family on 19th November 1828. Her father and mother were Moropant Tambe and Bhagirathi Tambe. The Queen of Jhansi grew up with proper military exposure under Tantia Tope and Nana Sahib. She is also popularly known as Manu among the Marathas."
+}
+function button9(){
+    document.getElementById("display_text").innerHTML="Shashi Tharoor is an Indian politician, diplomat and writer who serves as a Member of Parliament in the House of the People of the Indian Parliament, having formerly been the Under-Secretary General of the United Nations."
+}
+function button10(){
+    document.getElementById("display_text").innerHTML="Priyanka Chopra Jonas (pronounced [prɪˈjəŋka ˈtʃoːpɽa]; née Chopra; born 18 July 1982) is an Indian actress, model, and singer. The winner of the Miss World 2000 pageant, Chopra is one of India's highest-paid actresses. She has received numerous accolades, including two National Film Award and five Filmfare Awards."
+}
